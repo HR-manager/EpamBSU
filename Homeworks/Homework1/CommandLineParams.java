01a9303125973e9a37692193ae547b85a10474bd
MEOW\Never4never <annephemere@gmail.com>
2016-10-02 22:15:02 +0300

    Comments added.

--- Homeworks/Homework1/CommandLineParams.java
+++ Homeworks/Homework1/CommandLineParams.java
@@ -1,7 +1,15 @@
 /**
- * Created by Never4never on 02.10.2016.
+ * This class is for practicing in cmd work
+ * @author Petryakina
+ * @version 0.5
  */
 public class CommandLineParams {
+    /**
+     * This method outputs arguments from command line in reverse order
+     * @author Petryakina
+     * @version 0.5
+     * @param args Arguments from command line
+     */
         public static void main(String[] args) {
             for(int i = args.length-1; i >= 0; i--) {
                 System.out.println("Argument " + i + "= " + args[i]);
