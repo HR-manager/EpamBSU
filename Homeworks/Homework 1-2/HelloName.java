37cb6deb128b764077f7762c9eff505402594510
MEOW\Never4never <annephemere@gmail.com>
2016-10-03 00:44:17 +0300

    task 2

---
+++ Homeworks/Homework 1-2/HelloName.java
@@ -0,0 +1,20 @@
+/**
+ * This class is also for practising cmd work.
+ * @author Petryakina
+ * @version 0.5
+ */
+
+public class HelloName {
+    /**
+     * This method outputs greetings to the name from command line.
+     *
+     * @param args Text (name) from command line
+     * @author Petryakina
+     * @version 0.5
+     */
+    public static void main (String[] args) {
+        for (int i = 0; i < 1; i++) {
+            System.out.println ("Hello, " + args[i]);
+        }
+    }
+}
